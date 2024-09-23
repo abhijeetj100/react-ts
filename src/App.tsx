57e61f801f643716b0071f80bdb6bdbe306b3918
Abhijeet Jain <abhijeetjain456@gmail.com>
2024-09-23 16:52:03 +0530

    Fix: codefactor code-smells

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Board from './components/TicTacToe/Board';
 import FilterableProductTable from './components/FilterableProductTable';
 
 function App() {
--- src/components/FilterableProductTable/index.tsx
+++ src/components/FilterableProductTable/index.tsx
@@ -1,4 +1,3 @@
-import Search from "antd/es/transfer/search";
 import ProductTable from "./ProductTable";
 import SearchTable from "./SearchBar";
 
@@ -9,14 +8,14 @@ interface IProduct{
     name: string;
 }
 
-const ProductData: IProduct[] = [
-    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-  ];
+// const ProductData: IProduct[] = [
+//     { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+//     { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+//     { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+//     { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+//     { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+//     { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+//   ];
 
 export default function FilterableProductTable(){
     return (
