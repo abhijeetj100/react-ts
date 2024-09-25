3519b615cd207afa379ff74ee887c17789867b9b
Abhijeet Jain <abhijeetjain456@gmail.com>
2024-09-26 01:28:58 +0530

    Chore: check for formatting using prettier in github action workflow

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import FilterableProductTable from "./components/FilterableProductTable"
 
 function App() {
     return (
-        <div className="App">
+        <div className='App'>
             <ConfigProvider
                 theme={{
                     algorithm: theme.defaultAlgorithm,
