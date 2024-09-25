bd818a2cf2e35bac05aee61ce4f389e4dc15e57d
Abhijeet Jain <abhijeetjain456@gmail.com>
2024-09-26 01:24:46 +0530

    Chore: Setup prettier

--- src/components/MyButton/index.tsx
+++ src/components/MyButton/index.tsx
@@ -1,20 +1,24 @@
-import {Button} from 'antd';
-import { MouseEventHandler } from 'react';
+import { Button } from "antd"
+import { MouseEventHandler } from "react"
 
 // function handleClick(count: number, setCount: Function){
 //     alert("Button Clicked");
 //     setCount(count+1);
 // }
 
-export default function MyButton({count, OnClick}: {count: number, OnClick: MouseEventHandler<HTMLButtonElement>}){
+export default function MyButton({
+    count,
+    OnClick,
+}: {
+    count: number
+    OnClick: MouseEventHandler<HTMLButtonElement>
+}) {
     // const [count, setCount] = useState(0);
-    console.log("Rendering MyButton"); // why is this being called twice?
+    console.log("Rendering MyButton") // why is this being called twice?
 
     // function handleClick(){
     //     setCount(count+1);
     //     console.log(`Button clicked ${count} times`);
     // }
-    return(
-        <Button onClick={OnClick}>Clicked {count} times</Button>
-    )
+    return <Button onClick={OnClick}>Clicked {count} times</Button>
 }
--- src/components/TicTacToe/index.tsx
+++ src/components/TicTacToe/index.tsx
@@ -1,9 +1,3 @@
 export default function TicTacToe() {
-    return (
-        <>
-            
-        </>
-
-
-    )
+    return <></>
 }
